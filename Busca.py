a516f67f496a3949b7458f82752853e8e44bab6f
Maria Luiza Pinheiro <102831221+MaluPinheiro@users.noreply.github.com>
2024-05-23 11:30:41 -0300

    Create Busca.py

---
+++ Busca.py
@@ -0,0 +1,51 @@
+import ctypes
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+    
+    def create_node(self, data):
+        return Node(data)
+    
+    def insert(self, data):
+        if self.root is None:
+            self.root = self.create_node(data)
+        else:
+            self._insert(data, self.root)
+    
+    def _insert(self, data, cur_node):
+        if data < cur_node.data:
+            if cur_node.left is None:
+                cur_node.left = self.create_node(data)
+            else:
+                self._insert(data, cur_node.left)
+        elif data > cur_node.data:
+            if cur_node.right is None:
+                cur_node.right = self.create_node(data)
+            else:
+                self._insert(data, cur_node.right)
+        else:
+            print("Valor já existe na Árvore!")
+    
+    def preorder(self, node):
+        if node is not None:
+            print(node.data, end=" ")
+            self.preorder(node.left)
+            self.preorder(node.right)
+    
+    def inorder(self, node):
+        if node is not None:
+            self.inorder(node.left)
+            print(node.data, end=" ")
+            self.inorder(node.right)
+
+    def postorder(self, node):
+        if node is not None:
+            self.postorder(node.left)
+            self.postorder(node.right)
+            print(node.data, end=" ")
